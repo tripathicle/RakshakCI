bce97392b685eca748cbd6d29a3e877f6a512aef
tripathicle <tripathicle@gmail.com>
2026-03-21 23:34:57 +0530

    storage-config

--- Environment/Dev/main.tf
+++ Environment/Dev/main.tf
@@ -4,3 +4,13 @@ module "rg" {
   rg_name  = var.rg_name
   location = var.location
 }
+
+module "StorageAccount" {
+  depends_on               = [module.rg]
+  source                   = "../../Modules/StorageAccount"
+  stg_name                 = var.stg_name
+  rg_name                  = var.rg_name
+  location                 = var.location
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
+}
--- Environment/Dev/vars.tf
+++ Environment/Dev/vars.tf
@@ -8,3 +8,20 @@ variable "location" {
   type        = string
 
 }
+
+variable "stg_name" {
+  description = "Name of the Storage Account"
+  type        = string
+
+}
+
+variable "account_tier" {
+  description = "The performance tier of the Storage Account. Valid values are: 'Standard' and 'Premium'."
+  type        = string
+
+}
+
+variable "account_replication_type" {
+  description = "The replication type of the Storage Account. Valid values are: 'LRS', 'GRS', 'RAGRS', and 'ZRS'."
+  type        = string
+}
